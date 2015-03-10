794b768406e18f6afd87bd819797d78563219f6f
Diego <diego@upperquad.com>
2015-03-09 22:16:43 -0700

    Scroll

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,10 +4,34 @@
   $(window).resize(handleResize);
   $(window).trigger('resize');
 
+  var controller = new ScrollMagic.Controller();
+  var headerBgTween = TweenMax.to($('hero-bg'), {
+    y: '-100px'
+  });
+
+  var headerTxtTween = TweenMax.fromTo($('.hero-header', {
+
+  }, {
+
+  });
+
+  var headerTimeline = new TimelineMax();
+  headerTimeline.add({
+    headerBgTween,
+    headerTxtTween
+  });
+  var headerScene = new ScrollMagic({
+      duration: $('.')
+    })
+    .setTween(headerTimeline)
+    .addTo(controller);
+
+
   function handleResize() {
     $('.section').css( {
       'width': $(window).width(),
       'height': $(window).height()
-    })
+    });
+
   }
 }());
